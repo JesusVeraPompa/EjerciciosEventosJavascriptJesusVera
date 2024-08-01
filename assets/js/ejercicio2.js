1ba8cab039420841010d6e6da875ade423cdd9ba
JesusVeraPompa <merkapompa@gmail.com>
2024-08-01 13:07:47 -0500

    1.2 Completado el  Ejercicio 2

--- assets/js/ejercicio2.js
+++ assets/js/ejercicio2.js
@@ -1,5 +1,27 @@
 //  Ejercicio 2
 
-//  Captamos el Valor de lo Pesos
+let valorCambio = 4047.85;
+
+//  Captamos el Valor de los Pesos
 let valorPesos = document.getElementById("pesos").value;
-console.log(valorPesos);
+//console.log(valorPesos);
+
+//  Captamos el Valor de los Dolares
+let valorDolar = document.getElementById("dolar").value;
+//console.log(valorDolar);
+
+//  Generamos la Funcion para Cambiar de Pesos a Dolares
+let nuevoValorPesos = document.getElementById("pesos");
+nuevoValorPesos.addEventListener("keyup", (e) => {
+    //console.log(e.target.value);
+    let valor = e.target.value / valorCambio;
+    document.getElementById("dolar").value = Intl.NumberFormat("de-DE").format(valor.toFixed(2));
+});
+
+//  Generamos la Funcion para Cambiar de Dolares a Pesos
+let nuevoValorDolar = document.getElementById("dolar");
+nuevoValorDolar.addEventListener("keyup", (e) => {
+    //console.log(e.target.value);
+    let valor = e.target.value * valorCambio;
+    document.getElementById("pesos").value = Intl.NumberFormat("de-DE").format(valor.toFixed(2));
+});
